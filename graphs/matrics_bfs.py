87a33f1e4c7f58a166ae43f1ce6b54385f03eb02
hunny <hunnyjain711@gmail.com>
2023-01-20 00:26:40 +0530

    Matrics BFS fix

--- graphs/matrics_bfs.py
+++ graphs/matrics_bfs.py
@@ -16,14 +16,12 @@ def bfs(grid):
                 return length
             neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]]
             for dr, dc in neighbors:
-                if min(dr + r, dc + c) or r + dr == rows or c + dc == cols or \
-                        (r + dr, c + dc) in visit or grid[r + dr][c + dc] == 1:
+                if (min(dr + r, dc + c) < 0 or r + dr == rows or c + dc == cols or (r + dr, c + dc) in visit or
+                        grid[r + dr][c + dc] == 1):
                     continue
                 queue.append((r + dr, c + dc))
                 visit.add((r + dr, c + dc))
         length += 1
-
-    return length
 
 
 matrics = [[0, 0, 0, 0],
