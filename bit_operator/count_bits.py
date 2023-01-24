f77cc2d98ad946534d1154272000fb6449849de5
hunny <hunnyjain711@gmail.com>
2023-01-24 23:26:41 +0530

    Bit Operators

---
+++ bit_operator/count_bits.py
@@ -0,0 +1,22 @@
+def count_bits(n):
+    count = 0
+    while n > 0:
+        count += 1
+        n = n >> 1
+    return count
+
+
+print(count_bits(23))
+
+
+# count numbers of 1 bits
+def count_1_bits(n):
+    count = 0
+    while n > 0:
+        if n & 1 == 1:
+            count += 1
+        n = n >> 1  # Bit shifting
+    return count
+
+
+print(count_1_bits(23))
