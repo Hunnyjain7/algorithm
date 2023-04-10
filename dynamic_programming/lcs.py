2a097861c05a35b30a10679de25fd898194f38f5
hunny <hunnyjain711@gmail.com>
2023-04-10 23:37:04 +0530

    Knapsack, unbounded_knapsack and lcs

---
+++ dynamic_programming/lcs.py
@@ -0,0 +1,46 @@
+def dfs(s1, s2, i1, i2):
+    if i1 >= len(s1) or i2 >= len(s2):
+        return 0
+
+    if s1[i1] == s2[i2]:
+        return 1 + dfs(s1, s2, i1 + 1, i2 + 1)
+    else:
+        return max(dfs(s1, s2, i1 + 1, i2), dfs(s1, s2, i1, i2 + 1))
+
+
+def lcs(s1, s2):
+    return dfs(s1, s2, 0, 0)
+
+
+def dfs2(s1, s2, i1, i2, cache):
+    if i1 >= len(s1) or i2 >= len(s2):
+        return 0
+    if cache[i1][i2] != -1:
+        return cache[i1][i2]
+
+    if s1[i1] == s2[i2]:
+        cache[i1][i2] = 1 + dfs(s1, s2, i1 + 1, i2 + 1)
+    else:
+        cache[i1][i2] = max(dfs(s1, s2, i1 + 1, i2), dfs(s1, s2, i1, i2 + 1))
+    return cache[i1][i2]
+
+
+def memoization_lcs(s1, s2):
+    cache = [[-1] * len(s2) for i in range(len(s1))]
+    return dfs2(s1, s2, 0, 0, cache)
+
+
+def dp(s1, s2):
+    cache = [[0] * (len(s2) + 1) for i in range((len(s1) + 1))]
+
+    for r in range(len(s1)):
+        for c in range(len(s2)):
+            if s1[r] == s2[c]:
+                cache[1 + r][1 + c] = 1 + cache[r][c]
+            else:
+                cache[1 + r][1 + c] = max(cache[r][1 + c], cache[1 + r][c])
+    return cache[-1][-1]
+
+
+if __name__ == '__main__':
+    print(dp("ADCB", "ABC"))
--- dynamic_programming/unbounded_knapsack.py
+++ dynamic_programming/unbounded_knapsack.py
@@ -1,7 +1,6 @@
-"""
-Given a list of N items, and a backpack with a limited capacity, return the maximum total profit that
+""" Given a list of N items, and a backpack with a limited capacity, return the maximum total profit that
 can be contained in the backpack. The i-th item's profit is profit[i] and it's weight is weight[i].
-Assume you can only add each item to the bag at most one time.
+Assume you can have an unlimited number of each item available.
 """
 
 
@@ -9,41 +8,44 @@ def dfs(i, profit, weight, capacity):
     if i == len(profit):
         return 0
 
+    # Skip item i
     max_profit = dfs(i + 1, profit, weight, capacity)
 
+    # Include item i
     new_cap = capacity - weight[i]
     if new_cap >= 0:
-        p = profit[i] + dfs(i + 1, profit, weight, new_cap)
-        max_profit = max(max_profit, p)
+        p = profit[i] + dfs(i, profit, weight, new_cap)
+        # Compute the max
+        max_profit = max(p, max_profit)
     return max_profit
 
 
 # Brute force Solution
-# Time: O(2^n), Space: O(n)
-# Where n is the number of items.
-def knapsack(profit, weight, capacity):
+# Time: O(2^m), Space: O(m)
+# Where m is the capacity.
+def unbounded_knapsack(profit, weight, capacity):
     return dfs(0, profit, weight, capacity)
 
 
 def dfs2(i, profit, weight, capacity, cache):
     if i == len(profit):
         return 0
-    if cache[i][capacity] != -1:
+    if cache[i][capacity] != 0:
         return cache[i][capacity]
-    cache[i][capacity] = dfs2(i + 1, profit, weight, capacity, cache)
 
+    cache[i][capacity] = dfs(i + 1, profit, weight, capacity)
     new_cap = capacity - weight[i]
     if new_cap >= 0:
-        p = profit[i] + dfs2(i + 1, profit, weight, new_cap, cache)
-        cache[i][capacity] = max(cache[i][capacity], p)
+        p = profit[i] + dfs(i, profit, weight, new_cap)
+        cache[i][capacity] = max(p, cache[i][capacity])
     return cache[i][capacity]
 
 
 # Memoization Solution
 # Time: O(n * m), Space: O(n * m)
 # Where n is the number of items & m is the capacity.
-def memoization_knapsack(profit, weight, capacity):
-    cache = [[-1] * (capacity + 1) for _ in range(len(profit))]
+def memoization_unbounded_knapsack(profit, weight, capacity):
+    cache = [[0] * (capacity + 1) for _ in range(len(profit))]
     return dfs2(0, profit, weight, capacity, cache)
 
 
@@ -51,50 +53,42 @@ def memoization_knapsack(profit, weight, capacity):
 # Time: O(n * m), Space: O(n * m)
 # Where n is the number of items & m is the capacity.
 def dp(profit, weight, capacity):
-    profit_len = len(profit)
-    cache = [[0] * (capacity + 1) for i in range(profit_len)]
+    cache = [[0] * (capacity + 1) for _ in range(len(profit))]
 
     # Fill the first column and row to reduce edge cases
-    for i in range(len(cache)):
+    for i in range(len(profit)):
         cache[i][0] = 0
     for i in range(capacity + 1):
         if weight[0] <= i:
             cache[0][i] = profit[0]
 
-    for r in range(1, len(cache)):
-        for c in range(1, capacity + 1):
+    for r in range(len(cache)):
+        for c in range(capacity + 1):
             skip = cache[r - 1][c]
             include = 0
             new_cap = c - weight[r]
             if new_cap >= 0:
-                include = profit[r] + cache[r - 1][new_cap]
+                include = profit[r] + cache[r][new_cap]
             cache[r][c] = max(skip, include)
-
     return cache[-1][-1]
 
 
 # Memory optimized Dynamic Programming Solution
 # Time: O(n * m), Space: O(m)
 def optimized_dp(profit, weight, capacity):
-    profit_len = len(profit)
     prev_row = [0] * (capacity + 1)
 
-    # Fill the first row to reduce edge cases
-    for c in range(capacity + 1):
-        if weight[0] <= c:
-            prev_row[c] = profit[0]
-
-    for r in range(1, profit_len):
+    for r in range(len(profit)):
         curr_row = [0] * (capacity + 1)
-        for c in range(1, capacity + 1):
+        for c in range(capacity + 1):
             skip = prev_row[c]
             include = 0
             new_cap = c - weight[r]
             if new_cap >= 0:
-                include = profit[r] + prev_row[new_cap]
+                include = profit[r] + curr_row[new_cap]
             curr_row[c] = max(include, skip)
         prev_row = curr_row
-    return prev_row[capacity]
+    return prev_row[-1]
 
 
 if __name__ == '__main__':
